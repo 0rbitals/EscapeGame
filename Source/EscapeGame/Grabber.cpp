9a66f1748f878d5ba386baf570d1cbd271cfb2e1
Mahdi Sharif <electroniknight@gmail.com>
2017-03-01 11:38:05 +0000

    Added Grabber component

--- Source/EscapeGame/Grabber.cpp
+++ Source/EscapeGame/Grabber.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "EscapeGame.h"
-#include "PositionReporter.h"
+#include "Grabber.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UGrabber::UGrabber()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,19 +16,17 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString ObjectName = GetOwner()->GetName();
-	FVector ObjectPos = GetOwner()->GetActorLocation();
-	UE_LOG(LogTemp, Warning, TEXT("I'm %s at %s"), *ObjectName, *ObjectPos.ToString());
+	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
